9f92b7026c0ec4807662d0c9b9d0140fc49b24f8
brian <cds@sinosun.com.cn>
2020-01-06 14:19:57 +0800

    test

--- src/main/java/SayHello.java
+++ src/main/java/SayHello.java
@@ -2,5 +2,6 @@ public class SayHello {
 
     public static void main( String[] args ) {
         System.out.println( "Hello Git World!" );
+        System.out.println( "Hello Git World!" );
     }
 }
