509259b398c89ab56eac88cf257f02dd5307859b
cds <cds@BRIANCHEN-PC.sino.sz>
2020-08-07 10:27:08 +0800

    2020-08-07

--- src/main/java/SayHello.java
+++ src/main/java/SayHello.java
@@ -1,7 +1,10 @@
 public class SayHello {
 
+	public String getHello() {
+		return "hello";
+	}
     public static void main( String[] args ) {
         System.out.println( "Hello Git World!" );
-        System.out.println( "Hello Git World!" );
+        System.out.println( getHello() );
     }
 }
